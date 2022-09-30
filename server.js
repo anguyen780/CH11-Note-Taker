02fd31e8e5333a503102e69bfbf212d5e99b4301
Lux <anguyen780@gmail.com>
2022-09-29 19:51:50 -0500

    was able to get notes to save onto page as well as delete functionality

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -11,6 +12,38 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        var notes = JSON.parse(data);
+        res.json(notes);
+    });
+});
+
+app.post('/api/notes', (req, res) => {
+    let notes = req.body;
+    notes.id = uuidv4();
+    notePage.push(notes);
+    fs.writeFile('./db/db.json', JSON.stringify(notePage,null,2), (err) => {
+        if(err) throw err;
+        res.json(notes);
+    });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+   for (let i = 0; i < notePage.length; i++) {
+    if (notePage[i].id === req.params.id){
+        notePage.splice(i,1);
+        break;
+    }
+   }
+   fs.writeFile('./db/db.json', JSON.stringify(notePage), (err) => {
+    if (err) throw err;
+   })
+   res.json(notePage)
+});
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
@@ -18,24 +51,6 @@ app.get('/notes', (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
-
-app.get('/api/notes', (req, res) => {
-    fs.readFile('/db/db.json', "utf8", (err, data) => {
-        if (err) throw err;
-        res.json(notes);
-    });
-})
-
-app.post('/api/notes', (req, res) => {
-        let notes = req.body;
-        notes.id = uuidv4();
-        notePage.push(notes);
-    fs.writeFile('./db/db.json', JSON.stringify(notePage,null,2), (err) => {
-        if(err) throw err;
-        res.json(notePage);
-    });
-    res.sendFile(path.join(__dirname,'/public/notes.html'));
-})
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`);
